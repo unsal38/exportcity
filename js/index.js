22d8804d729d3f9fc9cee73fef105eaad11eda28
unsal38 <53699505+unsal38@users.noreply.github.com>
2024-07-14 23:07:11 +0300

    update

--- js/index.js
+++ js/index.js
@@ -1,5 +1,39 @@
+// NAW BAYRAK GİZLE GÖSTER
 $(function () {
     const lang = $("html")[0].lang
     $("div .flag-naw ." + lang).removeClass("d-none")
-    // console.log("div .flag-naw ." + lang)
+})
+//SWİPER
+$(function () {
+    var swiper1 = new Swiper(".mySwiper-urun", {
+        slidesPerView: 4,
+        centeredSlides: true,
+        spaceBetween: 30,
+        grabCursor: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+          },
+      });
+
+      var swiper = new Swiper(".mySwiper-ref", {
+        spaceBetween: 10,
+        slidesPerView: 4,
+        freeMode: true,
+        watchSlidesProgress: true,
+      });
+      var swiper2 = new Swiper(".mySwiper2-ref", {
+        spaceBetween: 10,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        thumbs: {
+          swiper: swiper,
+        },
+      });
 })
